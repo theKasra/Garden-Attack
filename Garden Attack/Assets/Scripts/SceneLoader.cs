efb8dfab526cf5de0878ab4bc2e4a5c7f7195a91
theKasra <thekasra@outlook.com>
2020-03-17 23:56:32 +0330

    Playspace Setup

--- Garden Attack/Assets/Scripts/SceneLoader.cs
+++ Garden Attack/Assets/Scripts/SceneLoader.cs
@@ -30,4 +30,9 @@ public class SceneLoader : MonoBehaviour
         SceneManager.LoadScene(currentSceneIndex + 1);
     }
 
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
 }
