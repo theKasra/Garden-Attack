6b2f20a2968e71a53549b8928b1ed602a930b0df
theKasra <thekasra@outlook.com>
2020-03-22 07:41:17 +0430

    Health & Damage

--- Garden Attack/Assets/Scripts/Projectile.cs
+++ Garden Attack/Assets/Scripts/Projectile.cs
@@ -6,10 +6,18 @@ public class Projectile : MonoBehaviour
 {
 
     [Range(0f, 10f)] [SerializeField] float projectileSpeed = 1f;
+    [SerializeField] float damage = 50f;
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.right * projectileSpeed * Time.deltaTime);
     }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var health = otherCollider.GetComponent<Health>();
+        health.DealDamage(damage);
+    }
+
 }
