ba0b90d8689f785260f25a7cbd831d5c9f31acba
theKasra <thekasra@outlook.com>
2020-03-23 09:19:51 +0430

    Defender selection UI

---
+++ Garden Attack/Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        var buttons = FindObjectsOfType<DefenderButton>();
+
+        foreach(DefenderButton button in buttons)
+        {
+            button.GetComponent<SpriteRenderer>().color = new Color32(68, 68, 68, 255);
+        }
+
+        GetComponent<SpriteRenderer>().color  = Color.white;
+    }
+
+}
