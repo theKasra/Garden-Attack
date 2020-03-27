f74fcd606c42a01e794924a775f84d1ba9c1d31b
theKasra <thekasra@outlook.com>
2020-03-27 22:16:25 +0430

    Spending stars on defenders

--- Garden Attack/Assets/Scripts/Defender.cs
+++ Garden Attack/Assets/Scripts/Defender.cs
@@ -6,6 +6,11 @@ public class Defender : MonoBehaviour
 {
     [SerializeField] int starCost = 100;
 
+    public int GetStarCost()
+    {
+        return starCost;
+    }
+
     public void AddStars(int amount)
     {
         FindObjectOfType<StarDisplay>().AddStars(amount);
