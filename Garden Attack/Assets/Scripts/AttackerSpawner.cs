75d9fed58b9c6b2e82121e5ab5bea5d99f1770b7
theKasra <thekasra@outlook.com>
2020-03-28 10:12:52 +0430

    Instantiate under Spawners (as child)

--- Garden Attack/Assets/Scripts/AttackerSpawner.cs
+++ Garden Attack/Assets/Scripts/AttackerSpawner.cs
@@ -22,7 +22,10 @@ public class AttackerSpawner : MonoBehaviour
 
     private void SpawnAttacker()
     {
-        Instantiate(attackerPrefab, transform.position, Quaternion.identity);
+        Attacker newAttacker = Instantiate
+            (attackerPrefab, transform.position, Quaternion.identity)
+            as Attacker;
+        newAttacker.transform.parent = transform;
     }
 
     // Update is called once per frame
