1e67f9792570d079df5b1efe14f5be69bacbda15
theKasra <thekasra@outlook.com>
2020-03-28 11:48:35 +0430

    Change animation state

--- Garden Attack/Assets/Scripts/Shooter.cs
+++ Garden Attack/Assets/Scripts/Shooter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,57 @@ public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectile;
     [SerializeField] GameObject gun;
+    AttackerSpawner myLaneSpawner;
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        SetLaneSpawner();
+    }
+
+    private void Update()
+    {
+        if(IsAttackerInLane())
+        {
+            animator.SetBool("isAttacking", true);
+        }
+
+        else
+        {
+            animator.SetBool("isAttacking", false);
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        var spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach(AttackerSpawner spawner in spawners)
+        {
+            bool isCloseEnough = 
+                (Mathf.Abs(spawner.transform.position.y - transform.position.y) 
+                <= Mathf.Epsilon);
+
+            if(isCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if(myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+
+        else
+        {
+            return true;
+        }
+    }
 
     public void Fire()
     {
