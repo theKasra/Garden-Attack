988c16c280852f04c3ebe660a87d88dacd2cbe7d
theKasra <thekasra@outlook.com>
2020-04-06 11:30:49 +0430

    Fox Slider

---
+++ Garden Attack/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    [Tooltip("Level timer in seconds")]
+    [SerializeField] float levelTime = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Slider>().value = Time.timeSinceLevelLoad / levelTime;
+        bool timerFinished = (Time.timeSinceLevelLoad >= levelTime);
+        if(timerFinished)
+        {
+            Debug.Log("Expired!");
+        }
+    }
+}
